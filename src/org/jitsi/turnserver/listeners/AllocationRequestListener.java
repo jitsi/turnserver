657b1216ba2f53098930576202eed4f5fba4b3b6
Aakash Garg <aakashgargnsit@gmail.com>
2014-06-06 12:05:49 +0530

    Adds Allocation creation mechanism.

--- src/org/jitsi/turnserver/listeners/AllocationRequestListener.java
+++ src/org/jitsi/turnserver/listeners/AllocationRequestListener.java
@@ -13,6 +13,9 @@ import org.ice4j.*;
 import org.ice4j.attribute.*;
 import org.ice4j.message.*;
 import org.ice4j.stack.*;
+import org.jitsi.turnserver.stack.Allocation;
+import org.jitsi.turnserver.stack.FiveTuple;
+import org.jitsi.turnserver.stack.TurnStack;
 
 /**
  * The class that would be handling and responding to incoming Allocation
@@ -30,7 +33,7 @@ public class AllocationRequestListener
     private static final Logger logger = Logger
         .getLogger(AllocationRequestListener.class.getName());
 
-    private final StunStack stunStack;
+    private final TurnStack turnStack;
 
     /**
      * The indicator which determines whether this
@@ -41,11 +44,18 @@ public class AllocationRequestListener
     /**
      * Creates a new AllocationRequestListener
      * 
-     * @param stunStack
+     * @param turnStack
      */
     public AllocationRequestListener(StunStack stunStack)
     {
-        this.stunStack = stunStack;
+        if (stunStack instanceof TurnStack)
+        {
+            this.turnStack = (TurnStack) stunStack;
+        }
+        else
+        {
+            throw new IllegalArgumentException("This is not a TurnStack!");
+        }
     }
 
     @Override
@@ -59,6 +69,10 @@ public class AllocationRequestListener
         Message message = evt.getMessage();
         if (message.getMessageType() == Message.ALLOCATE_REQUEST)
         {
+            logger.setLevel(Level.FINEST);
+            logger.finest("Received a Allocation Request");
+            System.out.println("Received a Allocation Request");
+            
             Response response = null;
             RequestedTransportAttribute requestedTransportAttribute =
                 (RequestedTransportAttribute) message
@@ -72,9 +86,26 @@ public class AllocationRequestListener
                 (ReservationTokenAttribute) message
                     .getAttribute(Attribute.RESERVATION_TOKEN);
 
+            LifetimeAttribute lifetimeAttribute = 
+                (LifetimeAttribute) message.getAttribute(Attribute.LIFETIME);
+            
+            if (lifetimeAttribute == null)
+            {
+                lifetimeAttribute =
+                    AttributeFactory
+                        .createLifetimeAttribute(
+                            (int) (Allocation.MAX_LIFETIME / 1000));
+            }
+            
             EvenPortAttribute evenPortAttribute =
                 (EvenPortAttribute) message.getAttribute(Attribute.EVEN_PORT);
-
+            
+            TransportAddress clientAddress = evt.getRemoteAddress();
+            TransportAddress serverAddress = evt.getLocalAddress();
+            Transport transport = Transport.UDP;
+            FiveTuple fiveTuple =
+                new FiveTuple(clientAddress, serverAddress, transport);
+              
             Character errorCode = null;
             if (requestedTransportAttribute == null)
             {
@@ -82,6 +113,7 @@ public class AllocationRequestListener
             }
             else if (requestedTransportAttribute.getRequestedTransport() != 17)
             {
+               System.out.println(requestedTransportAttribute.getRequestedTransport());
                 errorCode = ErrorCodeAttribute.UNSUPPORTED_TRANSPORT_PROTOCOL;
             }
             else if (reservationTokenAttribute != null
@@ -89,30 +121,55 @@ public class AllocationRequestListener
             {
                 errorCode = ErrorCodeAttribute.BAD_REQUEST;
             }
+            
+            if (turnStack.getServerAllocation(fiveTuple)!=null)
+            {
+                errorCode = ErrorCodeAttribute.ALLOCATION_MISMATCH;
+            }
             // do other checks here
-            else if (errorCode != null)
+            
+            if (errorCode == null)
             {
                 // do the allocation with 5-tuple
-                TransportAddress mappedAddress = evt.getRemoteAddress();
-                /*
-                 * response = MessageFactory.createAllocationResponse(request,
-                 * mappedAddress, relayedAddress, lifetime);
-                 */
+                TransportAddress relayAddress = Allocation.getNewRelayAddress();
+                logger.finest("Added a new Relay Address "+relayAddress);
+                System.out.println("Added a new Relay Address "+relayAddress);
+                
+                Allocation allocation =
+                    new Allocation(relayAddress, fiveTuple,
+                        lifetimeAttribute.getLifetime());
+                this.turnStack.addNewServerAllocation(allocation);
+                logger.finest("Added a new Allocation");
+                System.out.println("Added a new Allocation");
+                
+                response = MessageFactory.createAllocationResponse(
+                     (Request) message,
+                     allocation.getFiveTuple().getClientTransportAddress(), 
+                     allocation.getRelayAddress(), 
+                     (int) allocation.getLifetime());
             }
             else
             {
+                
+                logger.finest("Error Code " + errorCode
+                    + " on Allocation Request");
+                System.out.println("Error Code " + (int) errorCode
+                    + " on Allocation Request");
                 response =
                     MessageFactory.createAllocationErrorResponse(errorCode);
             }
+            
             try
             {
-                stunStack.sendResponse(evt.getTransactionID().getBytes(),
-                    response, evt.getLocalAddress(), evt.getRemoteAddress());
+                turnStack.sendResponse(
+                    evt.getTransactionID().getBytes(), response,
+                    evt.getLocalAddress(), evt.getRemoteAddress());
             }
             catch (Exception e)
             {
-                logger.log(Level.INFO, "Failed to send " + response
-                    + " through " + evt.getLocalAddress(), e);
+                logger.log(
+                    Level.INFO, "Failed to send " + response + " through "
+                        + evt.getLocalAddress(), e);
                 // try to trigger a 500 response although if this one failed,
                 throw new RuntimeException("Failed to send a response", e);
             }
@@ -131,7 +188,7 @@ public class AllocationRequestListener
     {
         if (!started)
         {
-            stunStack.addRequestListener(this);
+            turnStack.addRequestListener(this);
             started = true;
         }
     }
@@ -143,7 +200,7 @@ public class AllocationRequestListener
      */
     public void stop()
     {
-        stunStack.removeRequestListener(this);
+        turnStack.removeRequestListener(this);
         started = false;
     }
 }
