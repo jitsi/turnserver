26900250b6e7a22562706e960a5776b653593489
Aakash Garg <aakashgargnsit@gmail.com>
2014-08-21 00:24:25 +0530

    Adds TCP server socket in TURN server.

--- src/org/jitsi/turnserver/stack/TurnServer.java
+++ src/org/jitsi/turnserver/stack/TurnServer.java
@@ -13,10 +13,11 @@ import java.util.logging.*;
 
 import org.ice4j.*;
 import org.ice4j.socket.*;
-import org.ice4j.stunclient.StunDiscoveryReport;
+import org.ice4j.stunclient.*;
+
 import org.jitsi.turnserver.*;
 import org.jitsi.turnserver.listeners.*;
-import org.jitsi.turnserver.turnClient.*;
+import org.jitsi.turnserver.turnClient.StunClient;
 
 /**
  * The class to run a Turn server.
@@ -33,23 +34,25 @@ public class TurnServer
 
     private TurnStack turnStack = null;
 
-    private IceUdpSocketWrapper sock;
+    private IceUdpSocketWrapper turnUdpSocket;
 
     private final ServerPeerUdpEventHandler peerUdpHandler;
-    
+
     private final ServerChannelDataEventHandler channelDataHandler;
-    
+
+    private IceSocketWrapper turnTcpServerSocket;
+
     public TurnServer(TransportAddress localUDPAddress)
     {
         this.localAddress = localUDPAddress;
         this.peerUdpHandler = new ServerPeerUdpEventHandler();
         this.channelDataHandler = new ServerChannelDataEventHandler();
-        
-        turnStack = new TurnStack(this.peerUdpHandler,this.channelDataHandler);
+
+        turnStack = new TurnStack(this.peerUdpHandler, this.channelDataHandler);
         System.out.println("Setting turnstack.");
         this.peerUdpHandler.setTurnStack(turnStack);
         this.channelDataHandler.setTurnStack(turnStack);
-        
+
         logger.info("Server initialized Waiting to be started");
     }
 
@@ -58,8 +61,8 @@ public class TurnServer
      */
     public static void main(String[] args) throws Exception
     {
-        //getPublicAddress();
-        //getLocalAddress();
+        // getPublicAddress();
+        // getLocalAddress();
         TransportAddress localAddress = null;
         if (args.length == 2)
         {
@@ -87,17 +90,18 @@ public class TurnServer
         System.out.print("Server public IP and port : ");
         System.out.println("127.0.0.1:3478");
     }
-    
-    public static void getPublicAddress() throws UnknownHostException, Exception
+
+    public static void getPublicAddress() throws UnknownHostException,
+        Exception
     {
         System.out.print("Server public IP and port : ");
         StunDiscoveryReport report =
             StunClient.getReport("stunserver.org", "3478",
                 InetAddress.getLocalHost().getHostAddress(), "3478");
         System.out.println(report.getPublicAddress());
-        
+
     }
-    
+
     /**
      * Function to start the server
      * 
@@ -110,31 +114,31 @@ public class TurnServer
         {
             throw new RuntimeException("Local address not initialized");
         }
-        sock =
+        turnUdpSocket =
             new IceUdpSocketWrapper(new SafeCloseDatagramSocket(localAddress));
         /*
-        BindingRequestListener listner = new BindingRequestListener(turnStack);
-        listner.start();
-         */ 
+         * BindingRequestListener listner = new
+         * BindingRequestListener(turnStack); listner.start();
+         */
         AllocationRequestListener allocationRequestListner =
             new AllocationRequestListener(turnStack);
-        ChannelBindRequestListener channelBindRequestListener = 
+        ChannelBindRequestListener channelBindRequestListener =
             new ChannelBindRequestListener(turnStack);
-        ConnectionBindRequestListener connectionBindRequestListener = 
+        ConnectionBindRequestListener connectionBindRequestListener =
             new ConnectionBindRequestListener(turnStack);
-        ConnectRequestListener connectRequestListener = 
+        ConnectRequestListener connectRequestListener =
             new ConnectRequestListener(turnStack);
-        CreatePermissionRequestListener createPermissionRequestListener = 
+        CreatePermissionRequestListener createPermissionRequestListener =
             new CreatePermissionRequestListener(turnStack);
-        RefreshRequestListener refreshRequestListener = 
+        RefreshRequestListener refreshRequestListener =
             new RefreshRequestListener(turnStack);
-        BindingRequestListener bindingRequestListener = 
-        	new BindingRequestListener(turnStack);
-        
-        SendIndicationListener sendIndListener = 
-        	new SendIndicationListener(turnStack);
+        BindingRequestListener bindingRequestListener =
+            new BindingRequestListener(turnStack);
+
+        SendIndicationListener sendIndListener =
+            new SendIndicationListener(turnStack);
         sendIndListener.setLocalAddress(localAddress);
-        
+
         allocationRequestListner.start();
         channelBindRequestListener.start();
         connectionBindRequestListener.start();
@@ -142,14 +146,42 @@ public class TurnServer
         createPermissionRequestListener.start();
         refreshRequestListener.start();
         bindingRequestListener.start();
-        
+
         sendIndListener.start();
+
+        System.out.println("Address - " + localAddress.getHostAddress() + ":"
+            + localAddress.getPort());
+      
+        ServerSocket tcpServerSocket =
+            new ServerSocket(localAddress.getPort());
         
-        turnStack.addSocket(sock);
+/*        Agent agent = new Agent();
+        agent.setStunStack(this.turnStack);
+        IceMediaStream stream = new IceMediaStream(agent,"Turn Server");
+        Component component =
+            new Component(Component.RTP, Transport.TCP, stream);
+        
+        sock2 = new IceTcpServerSocketWrapper(mySock,component);
+*/
+        System.out.println("Adding a TCP server socket - "
+            + tcpServerSocket.getLocalSocketAddress());
+/*        try
+        {
+            Thread.sleep(5000);
+        }
+        catch (InterruptedException e)
+        {
+            System.err.println("Unable to sleep thread");
+        }
+*/        turnTcpServerSocket =
+            new IceTcpServerSocketWrapper(tcpServerSocket, this.turnStack.getComponent());
+
+        turnStack.addSocket(turnUdpSocket);
+        turnStack.addSocket(turnTcpServerSocket);
         started = true;
         logger.info("Server started, listening on " + localAddress.getAddress()
             + ":" + localAddress.getPort());
-        
+
     }
 
     /**
@@ -160,9 +192,12 @@ public class TurnServer
         logger.info("Stopping server at " + localAddress.getAddress() + ":"
             + localAddress.getPort());
         turnStack.removeSocket(localAddress);
-        sock.close();
-        sock = null;
-
+        turnStack = null;
+        turnUdpSocket.close();
+        turnUdpSocket = null;
+        this.turnTcpServerSocket.close();
+        this.turnTcpServerSocket = null;
+        
         localAddress = null;
         this.started = false;
         logger.info("Server stopped");
@@ -174,13 +209,12 @@ public class TurnServer
     }
 
     @Override
-    protected void finalize() throws Throwable
+    public void finalize() throws Throwable
     {
         // to free resources by default if shutdown is not invoked before the
         // object is destructed
-        if (isStarted())
-        {
+        
             shutDown();
-        }
+        
     }
 }
