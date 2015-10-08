f2dd7686833b8462e24389c3cab20df3ff07fd4c
damencho <damencho@jitsi.org>
2015-10-08 15:46:39 -0500

    Updates the license headers.

--- src/org/jitsi/turnserver/turnClient/TcpPeer.java
+++ src/org/jitsi/turnserver/turnClient/TcpPeer.java
@@ -1,3 +1,21 @@
+/*
+ * TurnServer, the OpenSource Java Solution for TURN protocol. Maintained by the
+ * Jitsi community (http://jitsi.org).
+ *
+ * Copyright @ 2015 Atlassian Pty Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jitsi.turnserver.turnClient;
 
 import java.io.IOException;
@@ -19,7 +37,7 @@ public class TcpPeer
         System.out.println("successfully conneted from "
             + client.getLocalPort());
         IceTcpSocketWrapper mySock = new IceTcpSocketWrapper(client);
-        
+
         byte[] data = new byte[1500];
         DatagramPacket p = new DatagramPacket(data, 0);
         System.out.println("Waiting for packet.");
